ba41eac5a0315d7e778b8f8fe97e4002226fa1ad
pi <pi@raspberrypi.local>
2026-05-09 07:54:49 +0100

    build文件

---
+++ launcher/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_luwu_launcher_FILE /home/pi/luwu-os/launcher/build/luwu_launcher)
+set(__QT_DEPLOY_TARGET_luwu_launcher_TYPE EXECUTABLE)
---
+++ launcher/build/CMakeFiles/luwu_launcher.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/luwu_launcher.dir/link.d"
+  "CMakeFiles/luwu_launcher.dir/main.cpp.o"
+  "CMakeFiles/luwu_launcher.dir/main.cpp.o.d"
+  "luwu_launcher"
+  "luwu_launcher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/luwu_launcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
